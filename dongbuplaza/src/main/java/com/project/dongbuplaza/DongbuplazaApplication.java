126a5165fc1349a03bb0fdd58c637cdf486be80a
umth_shift <umth_shift@kmtccloud.onmicrosoft.com>
2022-08-24 19:50:48 +0900

    fixed untracked files

---
+++ dongbuplaza/src/main/java/com/project/dongbuplaza/DongbuplazaApplication.java
@@ -0,0 +1,13 @@
+package com.project.dongbuplaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongbuplazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongbuplazaApplication.class, args);
+	}
+
+}
